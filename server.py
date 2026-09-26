b4addb0edf947cf7911718ee3e02636e4fecac5e
enzodeluca73-debug <enzodeluca73@gmail.com>
2026-09-26 15:53:38 +0200

    Fix app.run host address in server.py

--- server.py
+++ server.py
@@ -48,5 +48,5 @@ def get_product(product_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)app.run(host="o.o.o.o", port=port)
+    app.run(host="0.0.0.0", port=port)app.
     
